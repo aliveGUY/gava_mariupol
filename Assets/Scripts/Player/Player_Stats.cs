5eb24509ce0952384b4f9ca3cab4b4efea830626
aliveGUY <ilyadubrivvin@gmail.com>
2022-07-12 16:55:24 +0300

    Setup stats

---
+++ Assets/Scripts/Player/Player_Stats.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Stats : ScriptableObject
+{
+    public int Hunger = 100;
+    public int Mental_Health = 100;
+}
