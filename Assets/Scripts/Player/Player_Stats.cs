754e9ede72fd950dafdc84da1a5e5a149e2ffd7a
aliveGUY <ilyadubrivvin@gmail.com>
2022-07-13 13:44:17 +0300

    setup

--- Assets/Scripts/Player/Player_Stats.cs
+++ Assets/Scripts/Player/Player_Stats.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+//changes
 public class Player_Stats : MonoBehaviour
 {
     public Player_Storage storage;
