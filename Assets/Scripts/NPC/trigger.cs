b988b754f7f742218660fc3a57d0607109584495
Геогрий Макаров <gera54332@gmail.com>
2022-07-13 14:35:47 +0300

    DialogChange

---
+++ Assets/Scripts/NPC/trigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigger : MonoBehaviour
+{
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    Dialogue controller = other.GetComponent<Dialogue>();
+
+    //    //if (Input.GetKeyDown(KeyCode.Space))
+    //    //{
+    //        Debug.Log("Work");
+    //        controller.ShowDialogue = true;
+    //    //}
+    //}
+}
