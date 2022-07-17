eb1a337342352026d8985075278f2e23bf604c4d
aliveGUY <ilyadubrivvin@gmail.com>
2022-07-18 02:40:43 +0300

    Add stats

--- Assets/Scripts/Player/Player_Stats.cs
+++ Assets/Scripts/Player/Player_Stats.cs
@@ -2,20 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 //changes
 public class Player_Stats : MonoBehaviour
 {
     public Player_Storage storage;
-    public Image panel;
+    public Image mental;
+    public GameObject hunger;
+    public TMP_Text number_hunger;
+
+    private int boundary = 100;
 
     void Start() {
         storage.mental_health = 1f;
+        storage.hunger = 100;
     }
 
     void Update(){
-        if(storage.mental_health > 0){
-            storage.mental_health -= 0.001f;
-            panel.fillAmount = storage.mental_health;
+        if(storage.hunger < 60) {
+            boundary = 60;
         }
+
+        if(storage.hunger < 30) {
+            boundary = 30;
+        }
+        if(storage.mental_health > boundary){
+            storage.mental_health -= 0.1f;
+            mental.fillAmount = storage.mental_health;
+        }
+
+        hunger_handler();
+    }
+
+    public void hunger_handler() {
+        if(storage.hunger > 0) {
+            storage.hunger -= 0.1f;
+            number_hunger.text = $"{storage.hunger}";
+        }
+
     }
 }
